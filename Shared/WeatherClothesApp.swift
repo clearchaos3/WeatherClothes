edabb6990e4ec03e4b9f86edb30d2e4c58c20c53
clearchaos3 <clearchaos3@gmail.com>
2020-07-02 19:39:49 -0500

    Initial Commit

---
+++ Shared/WeatherClothesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherClothesApp.swift
+//  Shared
+//
+//  Created by Ryan Lee on 7/2/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherClothesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
